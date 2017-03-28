06aae7d2097d894ed7a2ac497374b2e5e6d4339c
laurieannegagnonouellet <laurie.gagnon31@gmail.com>
2017-03-28 15:31:02 -0400

    controller

--- app/controller/controleurProfil.php
+++ app/controller/controleurProfil.php
@@ -2,6 +2,31 @@
 
 
 //Importation des modeles
-require_once("../model/modeleProfil.php");
+require_once('app/model/modeleUser.php');
+require_once('app/model/modeleProfil.php');
+
+$tonUsager = new ModeleProfil();
+$tonUsager = $tonUsager->profilUtilisateur($_GET['userID']);
+
+
+
+$_SESSION['userID'] = 2;
+
+
+
+
+//Gérer les différences de comportement entre notre profil, et le profil des autres.
+if($_SESSION['userID'] == $tonUsager->idUtilisateur){
+     //Le code si c'est le profil de l'utilisateur connecté
+    $profilUserActuel = true;
+    $title = "Ajout photo";
+    $checkAbonnement=false;
+   
+} else {
+    //Le code si c'est le profil d'un autre utilisateur
+    $profilUserActuel = false;
+    $title = "S'Abonner";
+    $checkAbonnement = $tonUsager->checkAbonnement($_SESSION['userID']);
+}
 
 ?>
