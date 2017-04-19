71a06c38f3ec72e601105722f996a49e72b00f4b
alys89 <pare.alice@gmail.com>
2017-04-19 17:42:45 -0400

    decouverte+ modifier profil refait

--- app/view/decouverte.php
+++ app/view/decouverte.php
@@ -19,42 +19,8 @@
     <meta charset="utf-8">
     <title>decouverte</title>
     <link href="app/assets/reset.css" rel="stylesheet">
-    <link href="app/assets/style-laurie.css" rel="stylesheet" />
     <link href="app/assets/style.css" rel="stylesheet">
-<<<<<<< HEAD
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-=======
-    <script src="app/assets/lib/jquery.min.js" ></script>
-    
-    <script>
-    
-        $(document).ready(function(){
-            $(".blockAllo").click(function(){
-                    var recetteID = this.dataset.recetteid;
-                    console.log("Je clique");
-                    $.ajax({
-                        url         : "traitementAjax.php",
-                        method      : "GET",
-                        data        : "selectPhoto&recetteID="+recetteID,
-                        contentType : "text/html;charset=utf-8;",   
-                        success     : function(data){
-                                        $(".contenuRecette").html(data);
-                                        $("#affichageRecette").removeClass("hidden")
-                                        },
-                        fail    : function(){
-                                        $(".contenuRecette").html("Oups! Cette recette n'existe pas!");
-                                        $("#affichageRecette").removeClass("hidden")
-                                        }
-                    
-                    });
-                    
-                })
-            
-        })
-    
-    </script>
-    
->>>>>>> origin/alice-v3
 </head>
 
     
@@ -71,7 +37,7 @@
     $commentaires=$this->modele->selectionnerCommentaires($decouverte['idPhoto']);
     $miam = $this->modele->selectionnerNombre('idUtilisateur','likes',false,NULL,true,$decouverte['idPhoto']);
    
-          $html= '<div class="blockAllo" data-recetteid="'.$decouverte['idPhoto'].'">
+          $html= '<div class="blockAllo">
                         <div class="allo">
                             <div class="alloNom">
                                 <h3>@'. $decouverte['nomUtilisateur'].'</h3>
@@ -108,19 +74,9 @@
        echo $html;                   
                         
     }
-    
-    
 
 //}
         ?>
-    
-     <div id="affichageRecette" class="popup hidden">
-                <div class="contenuRecette" style="overflow:hidden;">
-                
-                
-                </div>
-            
-            </div>
    
    </section>     
         <?php include_once'footer.php'?>
