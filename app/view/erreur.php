454e019ff67f5ca1243b9aff25a51b0dd65dc978
Dennessie <laurie.gagnon31@gmail.com>
2017-04-20 18:05:27 -0400

    pu vu erreur

--- app/view/erreur.php
+++
@@ -1,4 +0,0 @@
-<div class="erreur" style="position:absolute; top:75%;left:38%; margin:0 auto; z-index:3;">
-    <h1 style="color:white; font-size:20px;">Mauvais usager ou mot de passe</h1>
-</div>
-
--- app/view/erreuremail.php
+++
@@ -1,3 +0,0 @@
-<div class="erreur" style="position:absolute; top:83%;left:35%; margin:0 auto; z-index:3;">
-    <h1 style="color:white; font-size:20px;">Veuillez entrer une adresse courriel valide</h1>
-</div>
